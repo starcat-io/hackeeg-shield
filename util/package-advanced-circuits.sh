02101d1e355c9c57342c4488a7dcf788ec260a1b
Adam Feuer <adamf@pobox.com>
2013-12-28 10:28:02 -0800

    updated packaging script to help AC recognize files better

--- util/package-advanced-circuits.sh
+++ util/package-advanced-circuits.sh
@@ -10,10 +10,10 @@ cp $BOARD.agnd-layer2.ger $DIR/$BOARD.GND
 cp $BOARD.dvdd-layer3.ger $DIR/$BOARD.PWR
 cp $BOARD.bottomlayer.ger $DIR/$BOARD.BOT
 cp $BOARD.topsoldermask.ger $DIR/$BOARD.GTS
-cp $BOARD.bottomsoldermask.ger $DIR/$BOARD.GTL
+cp $BOARD.bottomsoldermask.ger $DIR/$BOARD.GBS
 cp $BOARD.topsilkscreen.ger $DIR/$BOARD.GTO
 cp $BOARD.bottomsilkscreen.ger $DIR/$BOARD.GBO
 cp $BOARD.boardoutline.ger $DIR
-cp $BOARD.drills.xln $DIR/$BOARD.DRL
+cp $BOARD.drills.xln $DIR/$BOARD.TAP
 zip -r $DIR-$DATE.zip $DIR
 
