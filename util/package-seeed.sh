ddf0bb92213860dc85360e8630561f5bb4fc49b6
Adam Feuer <adamf@pobox.com>
2014-01-09 15:57:10 -0800

    updated board revision to 1.1.1; update package-seeed script

--- util/package-seeed.sh
+++ util/package-seeed.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 DATE=`date +'%Y-%m-%d-%H%M'`
-BOARD="ADS1298-breakout"
-DIR="$BOARD-seeed-files"
+BOARD="hackeeg-shield"
+DIR="$BOARD-seeedstudio-files"
+rm -rf $DIR
 mkdir -p $DIR 
 cp $BOARD.toplayer.ger $DIR/$BOARD.GTL
+cp $BOARD.agnd-layer2.ger $DIR/$BOARD.G2L
+cp $BOARD.dvdd-layer3.ger $DIR/$BOARD.G3L
 cp $BOARD.bottomlayer.ger $DIR/$BOARD.GBL
 cp $BOARD.topsoldermask.ger $DIR/$BOARD.GTS
 cp $BOARD.bottomsoldermask.ger $DIR/$BOARD.GBS
